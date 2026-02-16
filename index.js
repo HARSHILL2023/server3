a0781ad5fe602591ad877880ca9225d8c5e625c7
HARSHIL2023 <harshil.patel.cg@gmail.com>
2026-02-16 11:17:14 +0530

    practising

--- index.js
+++ index.js
@@ -1,142 +1,35 @@
 const express = require("express");
 
 const app = express();
-app.use(express.json());
 
 const users = [
-  {
-    id: 1,
-    title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-    price: 109.95,
-    category: "men's clothing",
-    image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png",
-    rating: {
-      rate: 3.9,
-      count: 120,
-    },
-  },
-  {
-    id: 2,
-    title: "Mens Casual Premium Slim Fit T-Shirts ",
-    price: 22.3,
-    category: "men's clothing",
-    image:
-      "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_t.png",
-    rating: {
-      rate: 4.1,
-      count: 259,
-    },
-  },
-  {
-    id: 3,
-    title: "Cotton Jacket",
-    price: 55.99,
-    category: "women's clothing",
-    image: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_t.png",
-    rating: {
-      rate: 4.7,
-      count: 500,
-    },
-  },
+  { id: 1, name: "Arjun", role: "student" },
+  { id: 2, name: "Priyesha", role: "mentor" },
+  { id: 3, name: "Spino", role: "IGL" },
+  { id: 4, name: "Moltec", role: "Gammer" }
 ];
 
 app.get("/", (req, res) => {
-  res.send("Express server is running");
+  res.send("Hi! My name is Anand");
 });
 
-app.get("/all", (req, res) => {
-  console.log("products", users);
+app.get("/about", (req, res) => {
+  res.send("About page");
+});
+
+app.get("/users", (req, res) => {
   res.status(200).json(users);
 });
 
-app.get("/product", (req, res) => {
-  console.log("products", users);
-  res.status(200).json(users);
-});
-
-app.get("/product/:id", (req, res) => {
-  const productId = parseInt(req.params.id);
-  const user = users.find((u) => u.id === productId);
+app.get("/users/:id", (req, res) => {
+  const userId = Number(req.params.id);
+  const user = users.find(u => u.id === userId);
 
   if (!user) {
-    return res.status(404).json({ message: "Product not found" });
+    return res.status(404).json({ message: "User not found" });
   }
 
   res.status(200).json(user);
-});
-
-app.get("/category/:type", (req, res) => {
-  const categoryType = req.params.type;
-  
-  const decodedCategory = decodeURIComponent(categoryType);
-  
-  const filteredProducts = users.filter(u => u.category.toLowerCase() === decodedCategory.toLowerCase());
-
-  if (filteredProducts.length === 0) {
-    return res.status(404).json({ 
-      message: "No products found in this category",
-      requestedCategory: decodedCategory 
-    });
-  }
-
-  res.status(200).json({
-    category: decodedCategory,
-    count: filteredProducts.length,
-    products: filteredProducts
-  });
-});
-
-
-app.post("/products", (req, res) => {
-  if (Array.isArray(req.body)) {
-    req.body.forEach(product => {
-      users.push(product);
-    });
-
-    res.status(201).json({
-      message: `${req.body.length} products created successfully`,
-      products: req.body
-    });
-  } else {
-
-    const newproduct = {
-      id: req.body.id,
-      title: req.body.title,
-      price: req.body.price,
-      category: req.body.category,
-      image: req.body.image,
-      rating: req.body.rating,
-    };
-
-    users.push(newproduct);
-
-    res.status(201).json({
-      message: "Product created",
-      product: newproduct,
-    });
-  }
-});
-
-
-app.post("/product", (req, res) => {
-  const newproduct = {
-    id: req.body.id,
-    title: req.body.title,
-    price: req.body.price,
-    category: req.body.category,
-    image: req.body.image,
-    rating: {
-      rate: req.body.rate,
-      count: req.body.count
-    }
-  };
-
-  users.push(newproduct);
-
-  res.status(201).json({
-    message: "Product created",
-    product: newproduct,
-  });
 });
 
 app.listen(3000, () => {
